7b956dbdf4cd1ec254cb8f880b39b648c343d603
Dustin Rue <dustin.rue@10up.com>
2020-03-28 15:02:49 -0500

    attempt to do a parallel build

---
+++ bin/build-image.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+IMAGE=${1:?}
+
+docker build --pull --build-arg APP=${IMAGE} . -t guardiantheater/${IMAGE}:latest 
+
+if [ ${CI} ] && [ "${TRAVIS_BRANCH}" == "master" ]; then
+  docker tag guardiantheater/${IMAGE}:latest guardiantheater/${IMAGE}:${TRAVIS_BUILD_NUMBER}
+  docker push guardiantheater/${IMAGE}:${TRAVIS_BUILD_NUMBER}
+elif [ ! ${CI} ]; then
+  docker push guardiantheater/${IMAGE}:latest
+fi
